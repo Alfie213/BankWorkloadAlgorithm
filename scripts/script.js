cf75ed8ffa13edd31663fe7540eb3a2f87fe3b06
Alfie213 <arturbogoratov@gmail.com>
2023-10-14 11:26:50 +0300

    Delete comments from script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,13 @@
-// async function myFunction() {
-//   console.log('Function called after 5 seconds');
-//   await new Promise(resolve => setTimeout(resolve, 5000));
-//   console.log('5 seconds have passed');
-// }
-
-// myFunction();
-
-// const OperationType = require('./OperationType');
-// import {BankOperation} from './BankOperation.js'
-
-// function someFunc(){
-//   let obf = new BankOperation(199);
-//   console.log("im someFunc");
-// }
-
-// someFunc();
-
 import { BankQueue } from "./BankQueue.js";
+import { BankOperation } from "./BankOperation.js";
 
 console.log('program start');
 
+const bankOperations = [];
+const numBankOperations = 8
+for (let i = 0; i < numBankOperations; i++) {
+    const bankOperation = new BankOperation()
+}
+
 const bankQueue = new BankQueue(4, 2);
 bankQueue.StartHandleBankOperations();
-
-// console.log('program finish');
